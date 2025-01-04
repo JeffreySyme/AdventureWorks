a0f3954d461d55f3231a783deb259a96d7028676
Jeffrey Syme <jsyme16@gmail.com>
2025-01-04 14:37:55 -0500

    Product description entity configuration

--- AdventureWorks.Data/AdventureWorksDbContext.cs
+++ AdventureWorks.Data/AdventureWorksDbContext.cs
@@ -5,8 +5,10 @@ namespace AdventureWorks.Data;
 public class AdventureWorksDbContext(DbContextOptions<AdventureWorksDbContext> options) : DbContext(options)
 {
     public DbSet<ProductCategory> ProductCategories { get; set; }
+    public DbSet<ProductDescription> ProductDescriptions { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new ProductCategoriesConfiguration());
+        modelBuilder.ApplyConfiguration(new ProductDescriptionConfiguration());
     }
 }
---
+++ AdventureWorks.Data/Entities/ProductDescription.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AdventureWorks.Data.Entities;
+public class ProductDescription
+{
+    public int ProductDescriptionId { get; set; }
+    public string Description { get; set; }
+    public DateTime ModifiedDate { get; set; }
+}
+internal class ProductDescriptionConfiguration : IEntityTypeConfiguration<ProductDescription>
+{
+    public void Configure(EntityTypeBuilder<ProductDescription> builder)
+    {
+        builder.ToTable("ProductDescription", "SalesLT");
+        builder.HasKey(x => x.ProductDescriptionId);
+        builder.Property(x => x.ProductDescriptionId).HasColumnName("ProductDescriptionID");
+        builder.Property(x => x.Description).IsRequired().HasMaxLength(400);
+        builder.Property(x => x.ModifiedDate);
+    }
+}
