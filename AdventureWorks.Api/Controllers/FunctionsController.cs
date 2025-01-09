0b36189590f7e068e86a55bc4188b2a6adf2252b
Jeffrey Syme <jeffrey.syme@outlook.com>
2025-01-09 09:30:37 -0500

    Controller for getting products and their models.

---
+++ AdventureWorks.Api/Controllers/FunctionsController.cs
@@ -0,0 +1,13 @@
+﻿using AdventureWorks.Models;
+using AdventureWorks.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventureWorks.Api.Controllers;
+public class FunctionsController(IAdventureWorksServices services) : Controller
+{
+    [HttpPost]
+    public async Task<IActionResult> GetProducts([FromBody] GetProductsParams request)
+    {
+        return Ok(await services.GetProductsAsync(request));
+    }
+}
